cf86261072ec1f7c331bf2c920efd9c382538777
Yuval Levi <yuval.teltech@gmail.com>
2018-10-17 11:33:26 +0300

    update

--- AndroidStudioProjects/AutoKeep/app/src/main/java/com/autokeep/AutoKeep/UserSearchResult.java
+++ AndroidStudioProjects/AutoKeep/app/src/main/java/com/autokeep/AutoKeep/UserSearchResult.java
@@ -6,16 +6,18 @@ import java.util.ArrayList;
 
 public class UserSearchResult {
 
-    private ArrayList<VehicleModel> cars = new ArrayList<VehicleModel>();
+    private ArrayList <VehicleModel> cars;
 
     public UserSearchResult(ArrayList <VehicleModel> cars) {
         this.setCars(new VehicleModel(12345,"Fiat",2007,5,
                 1600,true));
+        this.setCars(new VehicleModel(99999, "Toyota", 2018, 5,
+                1600, true));
         this.cars = cars;
     }
 
 
-    public void setCars(VehicleModel car) {
+    private void setCars(VehicleModel car) {
         this.cars.add(car);
     }
 
