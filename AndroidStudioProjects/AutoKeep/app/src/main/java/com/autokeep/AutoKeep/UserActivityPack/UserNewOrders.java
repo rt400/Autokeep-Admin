12c52728c0c458b3769c1929f2444d358b690d10
Yuval Levi <yuval.teltech@gmail.com>
2018-11-17 21:37:39 +0200

    fix all bugs , ready for test..

--- AndroidStudioProjects/AutoKeep/app/src/main/java/com/autokeep/AutoKeep/UserActivityPack/UserNewOrders.java
+++ AndroidStudioProjects/AutoKeep/app/src/main/java/com/autokeep/AutoKeep/UserActivityPack/UserNewOrders.java
@@ -61,6 +61,7 @@ public class UserNewOrders extends AppCompatActivity {
                         android.R.style.Theme_Holo_Light_Dialog_MinWidth,
                         mDateSetListener_Start,
                         year, month, day);
+                dialog.getDatePicker().setMinDate(System.currentTimeMillis() - 1000);
                 dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
                 dialog.show();
             }
@@ -78,6 +79,7 @@ public class UserNewOrders extends AppCompatActivity {
                         android.R.style.Theme_Holo_Light_Dialog_MinWidth,
                         mDateSetListener_End,
                         year, month, day);
+                dialog.getDatePicker().setMinDate(System.currentTimeMillis() - 1000);
                 dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
                 dialog.show();
             }
